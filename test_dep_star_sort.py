67eaca1a35419ee4bd6b8eb8315cd9df810bd760
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-12-31 21:58:23 +0900

    chore: fix indent

--- test_dep_star_sort.py
+++ test_dep_star_sort.py
@@ -2,17 +2,17 @@ import unittest
 import dep_star_sort
 
 class DepStarSortTest(unittest.TestCase):
-  def setUp(self) -> None:
+    def setUp(self) -> None:
       self.executor = dep_star_sort.DepStarSort("https://github.com/github/view_component")
 
-  def tearDown(self) -> None:
-      pass
+    def tearDown(self) -> None:
+        pass
 
-  def test_dep_url(self):
-      self.assertEqual(self.executor.dep_url(), "https://github.com/github/view_component/network/dependents")
+    def test_dep_url(self):
+        self.assertEqual(self.executor.dep_url(), "https://github.com/github/view_component/network/dependents")
 
-  def test_response_code(self):
-      self.assertEqual(self.executor.response_code(), 200)
+    def test_response_code(self):
+        self.assertEqual(self.executor.response_code(), 200)
 
 if __name__ == "__main__":
     unittest.main()
