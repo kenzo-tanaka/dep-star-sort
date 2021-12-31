ed9e2744e0b31d74eadf6b248621af73e704cbcc
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-12-31 21:54:13 +0900

    test: テストで使用するインスタンスの共通化

--- test_dep_star_sort.py
+++ test_dep_star_sort.py
@@ -3,20 +3,16 @@ import dep_star_sort
 
 class DepStarSortTest(unittest.TestCase):
   def setUp(self) -> None:
-      pass
+      self.executor = dep_star_sort.DepStarSort("https://github.com/github/view_component")
 
   def tearDown(self) -> None:
       pass
 
   def test_dep_url(self):
-      executor = dep_star_sort.DepStarSort("https://github.com/github/view_component")
-      self.assertEqual(executor.dep_url(), "https://github.com/github/view_component/network/dependents")
+      self.assertEqual(self.executor.dep_url(), "https://github.com/github/view_component/network/dependents")
 
   def test_response_code(self):
-      executor = dep_star_sort.DepStarSort("https://github.com/github/view_component")
-      self.assertEqual(executor.response_code(), 200)
-
-
+      self.assertEqual(self.executor.response_code(), 200)
 
 if __name__ == "__main__":
     unittest.main()
