f611f859d68ae0ec5276d6913eee470e42c798da
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-01 21:25:51 +0900

    feat: formatterを使って出力成功🎉

--- lib/dependencies.py
+++ lib/dependencies.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 import dependency_page
+import md_formatter
 
 class Dependencies:
     def __init__(self, github_url, min_star) -> None:
@@ -22,4 +23,5 @@ class Dependencies:
 if __name__ == "__main__":
     args = sys.argv
     dependencies = Dependencies(github_url=args[1], min_star=int(args[2]))
-    print(dependencies.popular_repos())
+    popular_repos = dependencies.popular_repos()
+    print(md_formatter.MdFormatter(popular_repos).display())
