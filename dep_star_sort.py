3f52549dffa71bb4f806516eb91833404e4c9d17
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-01 15:59:04 +0900

    fix: urlを引数にとってbeautifulsoupのセットアップをするメソッドの作成
    close #12

--- dep_star_sort.py
+++ dep_star_sort.py
@@ -8,7 +8,8 @@ class DepStarSort:
         self.dep_url = f"{self.github_url}/network/dependents"
 
     def next_page_link(self):
-        page_links = self.get_soup().find(attrs={"data-test-selector": "pagination"}).find_all('a')
+        # TODO: self.dep_url ではなく、ここも引数から受け取って渡す
+        page_links = self.soup(self.dep_url).find(attrs={"data-test-selector": "pagination"}).find_all('a')
         # Nextリンクのみ
         if len(page_links) == 1:
             return(page_links[0]['href'])
@@ -18,7 +19,7 @@ class DepStarSort:
     
     def evaluate_repos(self, min_star):
         result = []
-        for box in self.get_soup().find_all(class_="Box-row"):
+        for box in self.soup(self.dep_url).find_all(class_="Box-row"):
             repo = 'https://github.com' + box.find_all('a')[1]['href']
             star = int(box.find(class_='octicon-star').parent.text.replace('\n','').strip())
             if star >= min_star:
@@ -32,8 +33,8 @@ class DepStarSort:
         return(result)
 
     # TODO: private
-    def get_soup(self):
-        html = requests.get(self.dep_url)
+    def soup(self, url):
+        html = requests.get(url)
         return(BeautifulSoup(html.content, "html.parser"))
 
 if __name__ == "__main__":
--- test_dep_star_sort.py
+++ test_dep_star_sort.py
@@ -10,11 +10,11 @@ class DepStarSortTest(unittest.TestCase):
     def tearDown(self) -> None:
         pass
 
-    def _mock_get_soup(self):
+    def _mock_soup(self, url):
         with open('index.html') as f:
             return(BeautifulSoup(f, "html.parser"))
 
-    @mock.patch("dep_star_sort.DepStarSort.get_soup", new=_mock_get_soup)
+    @mock.patch("dep_star_sort.DepStarSort.soup", new=_mock_soup)
     def test_evaluate_repos(self):
         expect = [
             {
@@ -28,7 +28,7 @@ class DepStarSortTest(unittest.TestCase):
         ]
         self.assertEqual(self.executor.evaluate_repos(5), expect)
 
-    @mock.patch("dep_star_sort.DepStarSort.get_soup", new=_mock_get_soup)
+    @mock.patch("dep_star_sort.DepStarSort.soup", new=_mock_soup)
     def test_next_page_link(self):
         self.assertEqual(self.executor.next_page_link(), "https://github.com/github/view_component/network/dependents?dependents_after=MTgzNjk2NDY2MDM")
 
